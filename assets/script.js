d43f06f1e585e657fc8b8faee19222ea078a7927
GameMaster1928 <GameMaster1928@outlook.com>
2020-04-10 17:44:34 -0700

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+    document.getElementsByClassName("content")[0].style.opacity = "1";
+}
+
+function redirect(url) {
+    if (window.scrollY == 0) {
+        document.getElementsByClassName("content")[0].style.opacity = "0";
+        setTimeout(() => {
+            location = url;
+        }, 500);
+    } else {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+        setTimeout(() => {
+            document.getElementsByClassName("content")[0].style.opacity = "0";
+            setTimeout(() => {
+                location = url;
+            }, 500);
+        }, 500);
+    }
+}
+
+setTimeout(() => {
+    document.getElementsByClassName("content")[0].style.opacity = "1";
+}, 250);
